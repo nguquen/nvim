4eb008926d0ffddcf236f8c961c5fe8b3a962fea
nguquen <thienlduc@gmail.com>
2023-04-17 05:56:19 +0700

    update keys

--- lua/keys.lua
+++ lua/keys.lua
@@ -10,3 +10,19 @@ map('i', '<Up>', '<NOP>', { noremap = true })
 map('i', '<Down>', '<NOP>', { noremap = true })
 map('i', '<Left>', '<NOP>', { noremap = true })
 map('i', '<Right>', '<NOP>', { noremap = true })
+
+-- save & edit
+map('n', '<Leader>w', ':w<CR>', { noremap = true, silent = true })
+map('n', '<Leader>q', ':q<CR>', { noremap = true, silent = true })
+map('n', '<Leader>x', ':x<CR>', { noremap = true, silent = true })
+
+-- navigation
+map('n', '<Leader>h', '<C-W><C-H>', { noremap = true, silent = true })
+map('n', '<Leader>j', '<C-W><C-J>', { noremap = true, silent = true })
+map('n', '<Leader>k', '<C-W><C-K>', { noremap = true, silent = true })
+map('n', '<Leader>l', '<C-W><C-L>', { noremap = true, silent = true })
+
+-- misc
+map('n', '<Leader>ch', ':noh<CR>', { noremap = true, silent = true })
+map('n', '<Leader>p', '"0p', { noremap = true, silent = true })
+map('n', '<Leader>y', ':let @0=@*<CR>', { noremap = true, silent = true })
