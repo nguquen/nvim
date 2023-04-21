62c7c2b1488a907bed9a7b646e4990100001c461
nguquen <thienlduc@gmail.com>
2023-04-21 21:49:58 +0700

    config treesitter

--- init.lua
+++ init.lua
@@ -191,3 +191,14 @@ cmp.setup({
   },
 })
 
+-- treesitter
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "all",
+  sync_install = false,
+  auto_install = true,
+  ignore_install = {},
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
