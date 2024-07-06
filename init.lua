480b9adb0104e74d5e68ab3bbe4c4d88e8b652b9
nguquen <thienlduc@gmail.com>
2024-07-07 04:44:14 +0700

    [lsp-buffer] all

--- init.lua
+++ init.lua
@@ -505,7 +505,14 @@ cmp.setup({
     { name = 'nvim_lua' },
     { name = 'crates' },
     { name = 'vsnip' },
-    { name = 'buffer' },
+    {
+      name = 'buffer',
+      option = {
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end,
+      },
+    },
     { name = 'vim-dadbod-completion', priority = 100 },
   },
   window = {
