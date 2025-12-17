00a6ea6e4535b718ec1c27455eef30e7aa144a96
nguquen <thienlduc@gmail.com>
2025-12-17 20:54:24 +0700

    codecompanion: upgrade

--- init.lua
+++ init.lua
@@ -385,7 +385,7 @@ require('minuet').setup({
 })
 
 require('codecompanion').setup({
-  strategies = {
+  interactions = {
     chat = {
       adapter = 'copilot',
       -- adapter = 'ollama',
@@ -410,78 +410,84 @@ require('codecompanion').setup({
       adapter = 'copilot',
       -- adapter = 'ollama',
     },
-    agent = {
+    cmd = {
+      adapter = 'copilot',
+      -- adapter = 'ollama',
+    },
+    background = {
       adapter = 'copilot',
       -- adapter = 'ollama',
     },
   },
   adapters = {
     opts = {
-      show_defaults = false,
+      show_presets = false,
     },
-    ollama = function()
-      return require('codecompanion.adapters').extend('ollama', {
-        name = 'ollama',
-        formatted_name = 'Ollama',
-        env = {
-          url = 'http://127.0.0.1:11434',
-        },
-        headers = {
-          ['Content-Type'] = 'application/json',
-        },
-        schema = {
-          model = {
-            -- default = 'deepseek-r1:14b-qwen-distill-q4_K_M',
-            default = 'qwen2.5-coder:14b-instruct-q4_K_M',
+    http = {
+      ollama = function()
+        return require('codecompanion.adapters').extend('ollama', {
+          name = 'ollama',
+          formatted_name = 'Ollama',
+          env = {
+            url = 'http://127.0.0.1:11434',
           },
-        },
-        handlers = {
-          chat_output = function(self, data)
-            local output = {}
+          headers = {
+            ['Content-Type'] = 'application/json',
+          },
+          schema = {
+            model = {
+              -- default = 'deepseek-r1:14b-qwen-distill-q4_K_M',
+              default = 'qwen2.5-coder:14b-instruct-q4_K_M',
+            },
+          },
+          handlers = {
+            chat_output = function(self, data)
+              local output = {}
 
-            if data and data ~= '' then
-              if not self.opts.stream then
-                data = data.body
-              end
-              local ok, json = pcall(vim.json.decode, data, { luanil = { object = true } })
-
-              if not ok then
-                return { status = 'error' }
-              end
-
-              local message = json.message
-
-              if message.content then
-                if message.content:sub(1, 7) == '<think>' then
-                  output.content = '### Reasoning\n' .. message.content:sub(8)
-                elseif message.content:sub(-8) == '</think>' then
-                  output.content = message.content:sub(1, -9) .. '\n### Response'
-                else
-                  output.content = message.content
+              if data and data ~= '' then
+                if not self.opts.stream then
+                  data = data.body
                 end
-                output.role = message.role or nil
+                local ok, json = pcall(vim.json.decode, data, { luanil = { object = true } })
+
+                if not ok then
+                  return { status = 'error' }
+                end
+
+                local message = json.message
+
+                if message.content then
+                  if message.content:sub(1, 7) == '<think>' then
+                    output.content = '### Reasoning\n' .. message.content:sub(8)
+                  elseif message.content:sub(-8) == '</think>' then
+                    output.content = message.content:sub(1, -9) .. '\n### Response'
+                  else
+                    output.content = message.content
+                  end
+                  output.role = message.role or nil
+                end
+
+                return {
+                  status = 'success',
+                  output = output,
+                }
               end
 
-              return {
-                status = 'success',
-                output = output,
-              }
-            end
-
-            return nil
-          end,
-        },
-      })
-    end,
-    copilot = function()
-      return require('codecompanion.adapters').extend('copilot', {
-        schema = {
-          model = {
-            default = 'claude-haiku-4.5',
+              return nil
+            end,
           },
-        },
-      })
-    end,
+        })
+      end,
+      copilot = function()
+        return require('codecompanion.adapters').extend('copilot', {
+          schema = {
+            model = {
+              default = 'claude-haiku-4.5',
+            },
+          },
+        })
+      end,
+    },
   },
   display = {
     chat = {
