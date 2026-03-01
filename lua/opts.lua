24b07e24178026abbb186458c15f3e748f1e8fd8
nguquen <thienlduc@gmail.com>
2026-03-01 14:48:44 +0700

    clipboard: osc52

--- lua/opts.lua
+++ lua/opts.lua
@@ -6,6 +6,18 @@ local opt = vim.opt
 -- [[ clipboard ]]
 opt.clipboard = 'unnamedplus'
 -- opt.pastetoggle = '<F2>'
+---@diagnostic disable-next-line: inject-field
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
 
 -- [[ editor ]]
 opt.updatetime = 300
